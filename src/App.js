c846146687485c126b3b4c7875b5c2cd3b5fb10b
m2lovric <m2lovric@gmail.com>
2020-04-17 14:09:21 +0200

    modules

--- src/App.js
+++ src/App.js
@@ -1,62 +1,7 @@
 import React from 'react';
 import './App.scss';
-
-const Product = ({product, handleCart}) => {
-  return(
-    <article className="Product">
-      <img src={product.image} alt="product"/>
-      <p>{product.name}</p>
-      <p><span>{product.price.amount}</span> {product.price.currency}/{product.price.measureUnit}</p>
-      <Button product={product} handleCart={handleCart}/>
-    </article>
-  )
-}
-
-const CartProduct = ({cart, handleRemove}) => {
-  return(
-    <>
-      {
-        cart.map(el => {
-          return(
-            <article key={el.id}>
-              <img src={el.product.image} alt="product"/>
-              <div>
-                <p>{el.count}x {el.product.name}</p>
-                <p>Total: {(el.count * el.product.price.amount).toFixed(2)} {el.product.price.currency}</p>
-              </div>
-              <button className="btn" onClick={() => handleRemove(el.id)}> X </button>
-            </article>
-          )
-        })
-      }
-    </>
-  )
-}
-
-const Cart = ({cart, handleRemove}) => {
-  return(
-    <section className="Cart">
-      <h1>Cart</h1>
-      <CartProduct cart={cart} handleRemove={handleRemove} />
-      <h2>Total: {
-        cart.reduce((sum, el) => {
-          const total = parseFloat((el.count * el.product.price.amount).toFixed(2));
-          console.log(total);
-          return sum + total
-        }, 0).toFixed(2)
-      } Kn</h2>
-    </section>
-  )
-}
-
-const Button = ({product, handleCart}) => {
-  const add = () => {
-    handleCart(product.id, product)
-  }
-  return(
-    <button onClick={add}>Add to Cart</button>
-  )
-}
+import Product from './components/Product'
+import Cart from './components/Cart';
 
 class App extends React.Component {
   constructor(props){
